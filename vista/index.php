1daf0092c5ab725fe000f1a0d25d34ca49ea1524
MartRomeros <mart.romeros@duocuc.cl>
2025-05-19 17:54:21 -0400

    agregando css

--- vista/index.php
+++ vista/index.php
@@ -13,19 +13,62 @@ $productos = $data['DATOS'];
     <title>Ferremax</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 
 <body>
 
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Ferremax</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Dias feriados</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Comentarios</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 
     <main class="container-fluid">
         <!-- Parte del martin-->
-
+        <section class="row">
+            <?php
+            foreach ($productos as $producto => $value) {
+                echo '
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                ';
+            }
+            ?>
+        </section>
     </main>
+
     <main class="container-fluid">
         <!-- Parte del nicos-->
 
     </main>
+
     <main class="container-fluid">
         <!-- Parte del lukas-->
 
