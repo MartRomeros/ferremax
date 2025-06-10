a4b4e9041d73d4edb5831ae1ab7a61668511889c
benjamin nuñez <benj.nuneza@duocuc.cl>
2025-06-09 22:54:44 -0400

    cambio de nombre index en view, ahora se llama MainView

--- vista/MainView.php
+++ vista/MainView.php
@@ -13,7 +13,7 @@ $productos = $data['DATOS'];
     <title>Ferremax</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    <link rel="stylesheet" href="vista/css/estilos.css">
+    <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
 </head>
@@ -133,10 +133,10 @@ $productos = $data['DATOS'];
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
         crossorigin="anonymous"></script>
-    <script src="vista/js/apiMonedas.js"></script>
+    <script src="js/apiMonedas.js"></script>
     <!-- Enlaza tu script correctamente desde la carpeta 'js' -->
-    <script src="vista/js/apiUsuarios.js"></script>
-    <script src="vista/js/apiFeriados.js"></script>
+    <script src="js/apiUsuarios.js"></script>
+    <script src="js/apiFeriados.js"></script>
 </body>
 
 </html>
